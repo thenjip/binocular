eaaac3a4fe856763c7cea21eb7fdc0534c8b7d15
thenjip <39303840+thenjip@users.noreply.github.com>
2020-11-30 17:33:53 +0100

    Add lenses over PLens' members
    Move 'chain' lower in the module.
    Simplify the templates' body.

--- src/binocular/plens.nim
+++ src/binocular/plens.nim
@@ -47,7 +47,7 @@ template readStructType* [SR; R; W; SW](
 template readStructType* [SR; R; W; SW](
   self: PLens[SR, R, W, SW]
 ): typedesc[SR] =
-  self.typeof().readStructType()
+  SR
 
 
 template readMemberType* [SR; R; W; SW](
@@ -59,7 +59,7 @@ template readMemberType* [SR; R; W; SW](
 template readMemberType* [SR; R; W; SW](
   self: PLens[SR, R, W, SW]
 ): typedesc[R] =
-  self.typeof().readArgType()
+  R
 
 
 template writtenMemberType* [SR; R; W; SW](
@@ -71,7 +71,7 @@ template writtenMemberType* [SR; R; W; SW](
 template writtenMemberType* [SR; R; W; SW](
   self: PLens[SR, R, W, SW]
 ): typedesc[W] =
-  self.typeof().writtenMemberType()
+  W
 
 
 template writtenStructType* [SR; R; W; SW](
@@ -83,7 +83,7 @@ template writtenStructType* [SR; R; W; SW](
 template writtenStateType* [SR; R; W; SW](
   self: PLens[SR, R, W, SW]
 ): typedesc[SW] =
-  self.typeof().writtenStateType()
+  SW
 
 
 
@@ -92,7 +92,7 @@ template structType* [S; T](X: typedesc[Lens[S, T]]): typedesc[S] =
 
 
 template structType* [S; T](self: Lens[S, T]): typedesc[S] =
-  self.typeof().structType()
+  S
 
 
 template memberType* [S; T](X: typedesc[Lens[S, T]]): typedesc[T] =
@@ -100,7 +100,7 @@ template memberType* [S; T](X: typedesc[Lens[S, T]]): typedesc[T] =
 
 
 template memberType* [S; T](self: Lens[S, T]): typedesc[T] =
-  self.typeof().memberType()
+  T
 
 
 
@@ -109,13 +109,6 @@ func lens* [SR; R; W; SW](
   writer: MemberWriter[SR, W, SW]
 ): PLens[SR, R, W, SW] =
   PLens[SR, R, W, SW](reader: reader, writer: writer)
-
-
-
-func writer* [SR; R; W; SW](
-  self: PLens[SR, R, W, SW]
-): MemberWriter[SR, W, SW] =
-  self.writer
 
 
 
@@ -140,18 +133,6 @@ func modify* [SR; R; W; SW](
 
 
 
-func chain* [SR; R1; W1; SW; R2; W2](
-  self: PLens[SR, R1, W1, SW];
-  other: PLens[R1, R2, W2, W1]
-): PLens[SR, R2, W2, SW] =
-  lens(
-    self.read().map(other.read()),
-    (struct: SR, written: W2) =>
-      self.modify(other.write(() => written)).run(struct)
-  )
-
-
-
 proc read* [SR; R; W; SW](struct: SR; lens: PLens[SR, R, W, SW]): R =
   lens.read().run(struct)
 
@@ -170,6 +151,47 @@ proc modify* [SR; R; W; SW](
   f: R -> W
 ): SW =
   lens.modify(f).run(struct)
+
+
+
+func chain* [SR; R1; W1; SW; R2; W2](
+  self: PLens[SR, R1, W1, SW];
+  other: PLens[R1, R2, W2, W1]
+): PLens[SR, R2, W2, SW] =
+  lens(
+    self.read().map(other.read()),
+    (struct: SR, written: W2) => struct.modify(self, other.write(() => written))
+  )
+
+
+
+func reader* (
+  SR, R, W, SW: typedesc
+): Lens[PLens[SR, R, W, SW], MemberReader[SR, R]] =
+  lens(
+    read[SR, R, W, SW],
+    (self: PLens[SR, R, W, SW], reader: MemberReader[SR, R]) =>
+      lens(reader, self.writer)
+  )
+
+
+func writer* (
+  SR, R, W, SW: typedesc
+): Lens[PLens[SR, R, W, SW], MemberWriter[SR, W, SW]] =
+  lens(
+    (self: PLens[SR, R, W, SW]) => self.writer,
+    (self: PLens[SR, R, W, SW], writer: MemberWriter[SR, W, SW]) =>
+      lens(self.reader, writer)
+  )
+
+
+
+func reader* (S, T: typedesc): Lens[Lens[S, T], MemberReader[S, T]] =
+  reader(S, T, T, S)
+
+
+func writer* (S, T: typedesc): Lens[Lens[S, T], MemberWriter[S, T, S]] =
+  writer(S, T, T, S)
 
 
 
